49f144acf813b7a417a954015176e91fd452a20e
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-07-28 08:57:23 -0400

    HTML and logic updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,7 @@
-var friendsArray = [];
+var friendsArray = [{
+    name: 'Ben Bernanke',
+    photo: 'http://img.timeinc.net/time/daily/2012/1211/poy_benbernanke.jpg',
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}];
 
 module.exports = friendsArray;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -9,7 +9,44 @@ module.exports = function (app) {
     });
 
     app.post('/api/friends', function (req, res) {
-        friendsData.push(req.body);
+        // declare variables
+        var surveyData = req.body;
+        var scoreDelta = 0;
+
+        // initially you have no friends, but we'll fix that
+        var currentFriendMatch = {
+            name: "",
+            photo: "",
+            answerDelta: 100
+        };
+
+        // loop through friends array
+        for (var i = 0; i < friends.length; i++) {
+
+            // check the scores
+            for (var j = 0; j < friends[i].scores.length; j++) {
+
+                // calculate ∆ between scores; this is the "proprietary magic"
+                scoreDelta = scoreDelta + Math.abs(parseInt(friends[i].scores[j]) - parseInt(surveyData.scores[j]));
+
+                console.log(scoreDelta);
+
+                // add the potential best match to the current friend match var
+                if (scoreDelta <= currentFriendMatch.answerDelta) {
+                    currentFriendMatch.name = friends[i].name;
+                    currentFriendMatch.photo = friends[i].photo;
+                    currentFriendMatch.answerDelta = scoreDelta;
+                }
+            }
+            scoreDelta = 0;
+        }
+        console.log(currentFriendMatch.name);
+
+        // add a new object to the friends array
+        friends.push(surveyData);
+
+        // show your friend on the page
+        res.json(currentFriendMatch);
     });
 
 }
